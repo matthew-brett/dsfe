19872de6208073ffdeebcd98f589a42f8f331159
Matthew Brett <matthew.brett@gmail.com>
2018-10-03 21:48:18 +0100

    Refine clean command to leave .gitignore

--- scripts/clean.py
+++ scripts/clean.py
@@ -1,12 +1,14 @@
 import shutil as sh
-import os.path as op
+from os.path import abspath, dirname, join as pjoin
+from glob import glob
 
-path_root = op.join(op.dirname(op.abspath(__file__)), '..')
+path_root = pjoin(dirname(abspath(__file__)), '..')
 
-paths = [op.join(path_root, 'images', 'chapters'),
-         op.join(path_root, '_site'),
-         op.join(path_root, '_chapters'),
-         op.join(path_root, '_data', 'textbook.yml')]
+paths = [pjoin(path_root, 'images', 'chapters'),
+         pjoin(path_root, '_site'),
+         pjoin(path_root, '_data', 'textbook.yml')]
+# Clean directories and files, but not .gitignore, in _chapters
+paths += glob(pjoin(path_root, '_chapters', '*'))
 for path in paths:
     print('Removing {}...'.format(path))
     sh.rmtree(path, ignore_errors=True)
